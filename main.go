b4486246063949cb5b4e6052acd4f1d33c270f7f
Julie Larsen <00julie.larsen@gmail.com>
2022-02-28 16:06:59 +0100

    #first commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "myquote/myquote"
+
+func main() {
+	myquote.Glass()
+
+	myquote.Go()
+
+	myquote.Hello()
+
+	myquote.Opt()
+}
+ //main
---
+++ myquote/myquote.go
@@ -0,0 +1,26 @@
+package myquote
+
+import (
+
+	"rsc.io/quote"
+	"fmt"
+)
+
+//functions
+
+func Glass() {
+	fmt.Println(quote.Glass())
+}
+
+func Go() {
+	fmt.Println(quote.Go())
+}
+
+func Hello() {
+	fmt.Println(quote.Hello())
+}
+
+func Opt() {
+	fmt.Println(quote.Opt())
+}
+
